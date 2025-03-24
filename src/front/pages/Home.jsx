8ae0542625f17b58324f61ec67ae0cc68f4a6478
Ashley Dogan <a.a.rappette@gmail.com>
2025-03-24 22:41:52 +0000

    merging development

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react"
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import "../home.css";
+import newYorkImage from "../assets/img/newyork.jpg";
+
 
 export const Home = () => {
 
@@ -33,20 +35,63 @@ export const Home = () => {
 	}, [])
 
 	return (
-		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
-			<p className="lead">
-				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
-			</p>
-			<div className="alert alert-info">
-				{store.message ? (
-					<span>{store.message}</span>
-				) : (
-					<span className="text-danger">
-						Loading message from the backend (make sure your python 🐍 backend is running)...
-					</span>
-				)}
+		<>
+			<div className="container">
+				<div className="hotelsContainer">
+					<h1>Hotels</h1>
+					<div className="hotelCards">
+						<div className="card" id="newYorkCard" style={{ width: "18rem" }}>
+							<img src={newYorkImage} className="card-img-top" alt="New York" />
+							<div className="card-body">
+								<h5 className="card-title">New York</h5>
+								<a href="#" className="btn btn-primary">Find Hotels</a>
+							</div>
+						</div>
+						<div className="card" id="sanDiegoCard" style={{ width: "18rem" }}>
+							<img src={newYorkImage} className="card-img-top" alt="New York" />
+							<div className="card-body">
+								<h5 className="card-title">San Diego</h5>
+								<a href="#" className="btn btn-primary">Find Hotels</a>
+							</div>
+						</div>
+						<div className="card" id="parisCard" style={{ width: "18rem" }}>
+							<img src={newYorkImage} className="card-img-top" alt="New York" />
+							<div className="card-body">
+								<h5 className="card-title">Paris</h5>
+								<a href="#" className="btn btn-primary">Find Hotels</a>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<div className="attractionsContainer">
+					<h1>Attractions</h1>
+					<div className="attractionsCards">
+						<div className="card" id="diningCard" style={{ width: "18rem" }}>
+							<img src={newYorkImage} className="card-img-top" alt="New York" />
+							<div className="card-body">
+								<h5 className="card-title">Dining</h5>
+								<a href="#" className="btn btn-primary">Explore Dining</a>
+							</div>
+						</div>
+						<div className="card" id="sitesCard" style={{ width: "18rem" }}>
+							<img src={newYorkImage} className="card-img-top" alt="New York" />
+							<div className="card-body">
+								<h5 className="card-title">Sites</h5>
+								<a href="#" className="btn btn-primary">Explore Sites</a>
+							</div>
+						</div>
+						<div className="card" id="activitiesCard" style={{ width: "18rem" }}>
+							<img src={newYorkImage} className="card-img-top" alt="New York" />
+							<div className="card-body">
+								<h5 className="card-title">Activities</h5>
+								<a href="#" className="btn btn-primary">Explore Activities</a>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
+		</>
+
 	);
 }; 
