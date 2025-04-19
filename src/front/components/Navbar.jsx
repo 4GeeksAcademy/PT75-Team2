a7ff11d0e80ef0b56383611e950c0f8457a71fb0
Mario Orol <141705874+Mariorlando96@users.noreply.github.com>
2025-04-19 00:08:34 +0000

    fixing the navbar.jsx

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -20,7 +20,7 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-custom">
 			<div className="container d-flex justify-content-between align-items-center py-2">
-				<Link to={token ? "/home" : "/"} className="navbar-brand fs-3">
+				<Link to={"/"} className="navbar-brand fs-3">
 					<img
 						src={tripSyncLogo}
 						alt="tripSyncLogo"
